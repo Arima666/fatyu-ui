1b67882c8f9137da794a2f70996e94f299303e8b
Arima <zhuhuayu@tuzhanai.com>
2022-08-11 17:28:11 +0800

    feat: 增加 SFC 组件支持
    Vue 包不支持 SFC 组件，需要使用 @vitejs/plugin-vue 在编译时，处理 .vue 文件

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 
 import FYButton from "./button";
+import SFCButton from "./button/SFCButton.vue";
 
-createApp(FYButton).mount("#app");
+createApp(SFCButton).mount("#app");
